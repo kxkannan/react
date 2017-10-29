e898793253f9fd32351c3627cc3a9db0edc823e5
Krishnan Kannan <kxkannan@gmail.com>
2017-10-29 12:15:30 -0600

    updates ListContacts.js; added regex package

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
-function ListContacts(props) {
+class ListContacts extends Component {
+   static PropTypes = {
+     contacts: PropTypes.array.isRequired,
+     onDeleteContact: PropTypes.func.isRequired
+   }
+
+   state  = {
+     query: ''
+   }
+
+   updateQuery = (query) => {
+     this.setState({query: query.trim() })
+   }
+
+   clearQuery = () => {
+     this.setState({query: ''})
+   }
+
+
+   render() {
+     const { contacts, onDeleteContact} = this.props
+     const { query } = this.state
+     let showingContacts
+     if (this.state.query) {
+       const match = new RegExp(escapeRegExp(query), 'i')
+       showingContacts = contacts.filter((contact) => match.test(contact.name))
+     } else {
+       showingContacts = contacts
+     }
+
+     showingContacts.sort(sortBy('name'))
     return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+             className="search-contacts"
+             type="text"
+             placeholder="Search contacts"
+             value={query}
+             onChange={(event) => this.updateQuery(event.target.value)}
+             />
+        </div>
+
+        {showingContacts.length != contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show All</button>
+          </div>
+        )}
+
       <ol className='contact-list'>
-        {props.contacts.map( (contact) => (
+        {showingContacts.map( (contact) => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
                 backgroundImage: `url(${contact.avatarURL})`
@@ -16,13 +66,15 @@ function ListContacts(props) {
                 <p>{contact.email}</p>
               </div>
 
-              <button onClick={() => props.onDeleteContact(contact)} className='contact-remove'>
+              <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
                 Remove
               </button>
             </li>
         ))}
       </ol>
+      </div>
     )
+  }
 
 }
 
